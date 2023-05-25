eca5182a384fd6256615133649e9bdcc860938c0
Domingo Gallardo <domingo.gallardo@ua.es>
2023-05-25 15:32:10 +0200

    hasLocalBackup

--- Sources/SwiftWABackupAPI/SwiftWABackupAPI.swift
+++ Sources/SwiftWABackupAPI/SwiftWABackupAPI.swift
@@ -1,2 +1,12 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct WABackupAPI {
+    public static let defaultBackupPath = "~/Library/Application Support/MobileSync/Backup/"
+
+    public static func hasLocalBackup() -> Bool {
+        let fileManager = FileManager.default
+        let backupPath = NSString(string: defaultBackupPath).expandingTildeInPath
+        return fileManager.fileExists(atPath: backupPath)
+    }
+}
