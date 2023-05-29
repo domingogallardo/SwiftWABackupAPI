12365c930927e2302e74a772353329af134f93a3
Domingo Gallardo <domingo.gallardo@ua.es>
2023-05-29 11:10:44 +0200

    Refactored getLocalBackups() to return backup path and creation date.

--- Sources/SwiftWABackupAPI/SwiftWABackupAPI.swift
+++ Sources/SwiftWABackupAPI/SwiftWABackupAPI.swift
@@ -1,6 +1,11 @@
 
 import Foundation
 
+public struct BackupInfo {
+    public let path: String 
+    public let creationDate: Date
+}
+
 public struct WABackup {
     public static let defaultBackupPath = "~/Library/Application Support/MobileSync/Backup/"
 
@@ -12,17 +17,34 @@ public struct WABackup {
 
     // Needs permission to access ~/Library/Application Support/MobileSync/Backup/
     // Go to System Preferences -> Security & Privacy -> Full Disk Access
-    public static func getLocalBackups() -> [String]? {
+    public static func getLocalBackups() -> [BackupInfo]? {
         let fileManager = FileManager.default
         let backupPath = NSString(string: defaultBackupPath).expandingTildeInPath
         do {
-            return try fileManager.contentsOfDirectory(atPath: backupPath).filter {
-                isDirectory(at: backupPath + "/" + $0, with: fileManager) 
-            }
+            let directoryContents = try fileManager.contentsOfDirectory(atPath: backupPath)
+            return directoryContents.map { content in
+                let filePath = backupPath + "/" + content
+                return getBackupInfo(at: filePath, with: fileManager)
+            }.compactMap { $0 }
         } catch {
             print("Error while enumerating files \(backupPath): \(error.localizedDescription)")
             return nil
         }
+    }
+
+    private static func getBackupInfo(at path: String, with fileManager: FileManager) -> BackupInfo? {
+        if isDirectory(at: path, with: fileManager) {
+            do {
+                let attributes = try fileManager.attributesOfItem(atPath: path)
+                let creationDate = attributes[FileAttributeKey.creationDate] as? Date ?? Date()
+                let backupInfo = BackupInfo(path: path, creationDate: creationDate)
+                return backupInfo
+            } catch {
+                print("Error while getting backup info \(path): \(error.localizedDescription)")
+                return nil
+            }
+        }
+        return nil
     }
 
     private static func isDirectory(at path: String, with fileManager: FileManager) -> Bool {
